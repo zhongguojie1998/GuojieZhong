12950755aa488f477efa42272ea988982b33a67c
zhongguojie1998 <zhongguojie1998@gmail.com>
2026-01-08 16:18:09 -0500

    Add cv side bar menu

--- assets/js/common.js
+++ assets/js/common.js
@@ -6,4 +6,15 @@ $(document).ready(function() {
         $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
     });
     $('a').removeClass('waves-effect waves-light');
+
+    // bootstrap-toc
+    if ($("#toc-sidebar").length) {
+        var navSelector = "#toc-sidebar";
+        var $myNav = $(navSelector);
+        Toc.init($myNav);
+        $("body").scrollspy({
+            target: navSelector,
+            offset: 100,
+        });
+    }
 });
